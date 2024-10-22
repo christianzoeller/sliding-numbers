5165edae2ab4c3518cfede5ce92b595073b1f548
Christian Zöller <scat.07.tresen@icloud.com>
2024-10-22 21:55:30 +0200

    Update dependencies, target SDK, Gradle version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,12 +19,12 @@ keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 android {
     namespace = "christianzoeller.slidingnumbers"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "christianzoeller.slidingnumbers"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
